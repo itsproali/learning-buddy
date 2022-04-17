4b5dac346e0f41202ab51444b98deb7a50448d84
Mohammad Ali <itsproali@gmail.com>
2022-04-17 17:09:14 +0600

    Google Sign In Method Added

--- src/components/Shared/SocialLogin/SocialLogin.js
+++ src/components/Shared/SocialLogin/SocialLogin.js
@@ -1,16 +1,38 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../../firebase-init";
+import Spinner from "../Spinner/Spinner";
 import "./SocialLogin.css";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+
+  const [signInWithGoogle, user, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle();
+  };
+
+  if (googleLoading) {
+    return <Spinner></Spinner>;
+  }
+
+  if (user) {
+    navigate("/");
+  }
+
   return (
     <div>
+      {googleError && <p className="text-red-400">{googleError.message}</p>}
       <div className="or-container flex items-center justify-evenly my-4">
         <div className="or-line"></div>
         <p>Or</p>
         <div className="or-line"></div>
       </div>
 
-      <button className="social-login-group">
+      <button className="social-login-group" onClick={handleGoogleSignIn}>
         <img src="https://i.ibb.co/gRLDw5G/google-logo.png" alt="google-logo" />
         <p>Continue with Google</p>
       </button>
