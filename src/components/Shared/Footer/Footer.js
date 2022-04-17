3da304578cf056de283d479e40befed128e0eae4
Mohammad Ali <itsproali@gmail.com>
2022-04-17 12:25:59 +0600

    Footer Added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>This is the footer</h1>
-        </div>
-    );
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <div>
+      <div className="text-center mt-10 py-4">
+        &copy; Copyright {year} | Designed and Developed by{" "}
+        <a
+          className="text-yellow-500"
+          href="https://www.linkedin.com/in/itsproali/"
+        >
+          Mohammad Ali
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
